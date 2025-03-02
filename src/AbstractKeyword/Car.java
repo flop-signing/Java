05b40139e332f67e407394ff1f7c75e01cd74693
flop-signing <mehedy.mmc272@gmail.com>
2025-03-02 16:12:49 +0600

    Abstract Keyword

---
+++ src/AbstractKeyword/Car.java
@@ -0,0 +1,10 @@
+package AbstractKeyword;
+
+public abstract class Car {
+
+    public abstract void drive();
+
+    public void playMusic() {
+        System.out.println("Play music");
+    }
+}
---
+++ src/AbstractKeyword/WagonR.java
@@ -0,0 +1,7 @@
+package AbstractKeyword;
+
+public class WagonR extends Car{
+    public void drive() {
+        System.out.println("WagonR drive");
+    }
+}
